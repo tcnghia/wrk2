f90868fdbe3d665077917763f4b9a51c09e06e87
Joseph Burnett <josephburnett79@gmail.com>
2018-03-13 15:38:37 -0400

    Add points script for analysis.

---
+++ scripts/points.lua
@@ -0,0 +1,14 @@
+-- Emit time (seconds), latency (millis) data points prefixed with "===" (for grepping)
+
+local start
+local stop
+
+request = function()
+   start = os.clock()
+   return wrk.format()
+end
+
+response = function(status, headers, body)
+   stop = os.clock()
+   io.write("===" .. os.time() .. "," .. stop*1000-start*1000 .. "\n")
+end
