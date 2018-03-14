80d050adf9bcca0f515b86edd0808c0f985f9493
Joseph Burnett <josephburnett79@gmail.com>
2018-03-14 13:15:16 -0400

    Provide unique client ids. Include HTTP status in data points.

--- scripts/points.lua
+++ scripts/points.lua
@@ -3,12 +3,21 @@
 local start
 local stop
 
+math.randomseed(os.time())
+
+-- Keep track of when clients come online so total concurrent clients can be calculated.
+setup =  function(thread)
+   thread:set("id", "client-" .. math.floor(math.random() * 1000000))
+   io.write("===CLIENT_ONLINE=== " .. os.time() .. " " .. thread:get("id") .. " "  .. "\n")
+end
+
 request = function()
    start = os.clock()
    return wrk.format()
 end
 
+-- Emit each request as a time, latency data point.
 response = function(status, headers, body)
    stop = os.clock()
-   io.write("===" .. os.time() .. "," .. stop*1000-start*1000 .. "\n")
+   io.write("===DATA_POINT=== " .. os.time() .. " " .. stop*1000-start*1000 .. " " .. status .. "\n")
 end
